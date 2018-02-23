3deed053a053d43205d836e6b7813983eddc81cc
ggyarmat <ggyarmat@PMIILTLVL00186.PMINTL.NET>
2018-02-23 04:53:22 +0200

    removing h2 security

--- usermanagment/src/main/java/com/gabor/usermanagment/Main2.java
+++ usermanagment/src/main/java/com/gabor/usermanagment/Main2.java
@@ -2,9 +2,10 @@ package com.gabor.usermanagment;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.SecurityAutoConfiguration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class })
 @EnableJpaAuditing
 public class Main2 {
 
